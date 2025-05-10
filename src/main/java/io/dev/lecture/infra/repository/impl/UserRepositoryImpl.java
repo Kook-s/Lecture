b8f5503e2e980ce09fa8874a3370baf5c64b9648
Kook-s <kooks1126@naver.com>
2025-05-11 01:54:43 +0900

    feat : UserRepository 구현 및 예외 처리 추가

--- src/main/java/io/dev/lecture/infra/repository/impl/UserRepositoryImpl.java
+++ src/main/java/io/dev/lecture/infra/repository/impl/UserRepositoryImpl.java
@@ -4,6 +4,8 @@ import io.dev.lecture.domain.model.User;
 import io.dev.lecture.domain.repository.UserRepository;
 import io.dev.lecture.infra.Entity.UserEntity;
 import io.dev.lecture.infra.repository.UserJpaRepository;
+import io.dev.lecture.support.CustomException;
+import io.dev.lecture.support.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -19,11 +21,14 @@ public class UserRepositoryImpl implements UserRepository {
     public User getByUserid(long id) {
         return userJpaRepository.findById(id)
                 .map(UserEntity::toUser)
-                .get();
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
     @Override
     public List<User> findUserByIds(List<Long> userIds) {
-        return List.of();
+        return userJpaRepository.findAllById(userIds)
+                .stream()
+                .map(UserEntity::toUser)
+                .toList();
     }
 }
